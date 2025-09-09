af8371299b54d21ff47680874fee321412443642
agnair2004-ctrl <agnair2004@gmail.com>
2025-09-09 23:04:41 +0530

    Add files via upload

---
+++ 36th.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float a,b,c,d,r1,r2;
+    printf("Enter a b c: ");
+    scanf("%f %f %f",&a,&b,&c);
+    d=b*b-4*a*c;
+    if(d>0){
+        r1=(-b+sqrt(d))/(2*a);
+        r2=(-b-sqrt(d))/(2*a);
+        printf("Real roots: %.2f, %.2f\n",r1,r2);
+    } else if(d==0){
+        r1=r2=-b/(2*a);
+        printf("Equal roots: %.2f\n",r1);
+    } else {
+        printf("Imaginary roots\n");
+    }
+    return 0;
+}
