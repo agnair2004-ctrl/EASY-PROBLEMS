00d868d5bc6b2f8835e6c1a63629f461b3a49d3f
agnair2004-ctrl <agnair2004@gmail.com>
2025-09-09 23:03:55 +0530

    Add files via upload

---
+++ 34th.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n,sum=0;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    for(int i=1;i<n;i++)
+        if(n%i==0) sum+=i;
+    if(sum==n) printf("Perfect number\n");
+    else printf("Not perfect number\n");
+    return 0;
+}
